ef15f6ba2ade5a1fa4b856abf484b546adc76994
Andrew Courter <exosyphon@yahoo.com>
2023-09-25 21:21:53 -0500

    Wrap config in function

--- lua/telescope/_extensions/telescope-color-picker.lua
+++ lua/telescope/_extensions/telescope-color-picker.lua
@@ -1,8 +1,17 @@
+local has_telescope, telescope = pcall(require, "telescope")
+if not has_telescope then
+    error("telescope-color-picker.nvim requires telescope.nvim - https://github.com/nvim-telescope/telescope.nvim")
+end
+
+local colors = function()
+    require("telescope-color-picker").colors(require("telescope.themes").get_dropdown {})
+end
+
 return require("telescope").register_extension {
-  setup = function(ext_config, config)
-    -- access extension config and user config
-  end,
-  exports = {
-    colors = require "telescope-color-picker",
-  },
+    setup = function(ext_config, config)
+        -- access extension config and user config
+    end,
+    exports = {
+        colors = colors,
+    },
 }
