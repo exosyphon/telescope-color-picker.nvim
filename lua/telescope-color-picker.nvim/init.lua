e90e3231c922868558cf5281d74dc9f7a724e4f9
Andrew Courter <exosyphon@yahoo.com>
2023-09-25 21:02:06 -0500

    Update files to have correct extension structure

--- lua/telescope-color-picker.nvim/init.lua
+++ lua/telescope-color-picker.nvim/init.lua
@@ -1,19 +1,19 @@
 local M = {}
 
-M.choose_colors = function()
+M.colors = function(opts)
     local actions = require "telescope.actions"
     local actions_state = require "telescope.actions.state"
     local pickers = require "telescope.pickers"
     local finders = require "telescope.finders"
     local sorters = require "telescope.sorters"
-    local dropdown = require "telescope.themes".get_dropdown()
+    opts = opts or {}
 
     local function enter(bufnr)
         local selected = actions_state.get_selected_entry()
         local cmd = 'colorscheme ' .. selected[1]
         vim.cmd(cmd)
         local init_cmd = 'vim.cmd("' .. cmd .. '")'
-        local init_file = vim.fn.expand(vim.fn.stdpath('config') .. "/init.lua")
+        local init_file = vim.fn.expand(vim.fn.stdpath('config') .. "/colorscheme.lua")
         vim.fn.jobstart("if [[ $(cat " ..
             init_file ..
             " | grep -e 'vim.*cmd.*colorscheme') ]]; then sed -i -e /colorscheme/d " ..
@@ -37,22 +37,21 @@ M.choose_colors = function()
 
     local installedColors = vim.fn.getcompletion("", "color")
 
-    local opts = {
+    pickers.new(opts, {
         finder = finders.new_table(installedColors),
         sorter = sorters.get_generic_fuzzy_sorter({}),
 
         attach_mappings = function(_, map)
-            map("i", "<CR>", enter)
+            actions.select_default:replace(enter)
+
             map("i", "<C-j>", next_color)
+            map("i", "<C-k>", prev_color)
+            map("i", "<C-n>", next_color)
             map("i", "<C-k>", prev_color)
             return true
         end,
-
-    }
-
-    local colors = pickers.new(dropdown, opts)
-
-    colors:find()
+    }):find()
 end
 
+-- M.colors()
 return M
---
+++ lua/telescope/_extensions/telescope-color-picker.nvim.lua
@@ -0,0 +1,8 @@
+return require("telescope").register_extension {
+  setup = function(ext_config, config)
+    -- access extension config and user config
+  end,
+  exports = {
+    colors = require("telescope-color-picker.nvim").colors(require("telescope.themes").get_dropdown {})
+  },
+}
