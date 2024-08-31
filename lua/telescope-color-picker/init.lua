c098848b880d4ecdf643275f5b1edbe82f677bc2
AuroraOps04 <45115141+AuroraOps04@users.noreply.github.com>
2024-08-31 21:25:06 +0800

    Support windows (#2)
    * support windows
    
    * wirte file via lua
    
    * remove redundant code

--- lua/telescope-color-picker/init.lua
+++ lua/telescope-color-picker/init.lua
@@ -1,56 +1,67 @@
 local M = {}
 
 M.colors = function(opts)
-    local actions = require "telescope.actions"
-    local actions_state = require "telescope.actions.state"
-    local pickers = require "telescope.pickers"
-    local finders = require "telescope.finders"
-    local sorters = require "telescope.sorters"
-    opts = opts or {}
+  local actions = require "telescope.actions"
+  local actions_state = require "telescope.actions.state"
+  local pickers = require "telescope.pickers"
+  local finders = require "telescope.finders"
+  local sorters = require "telescope.sorters"
+  opts = opts or {}
 
-    local function enter(bufnr)
-        local selected = actions_state.get_selected_entry()
-        local cmd = 'colorscheme ' .. selected[1]
-        vim.cmd(cmd)
-        local init_cmd = 'vim.cmd("' .. cmd .. '")'
-        local init_file = vim.fn.expand(vim.fn.stdpath('config') .. "/init.lua")
-        vim.fn.jobstart("if [[ $(cat " ..
-            init_file ..
-            " | grep -e 'vim.*cmd.*colorscheme') ]]; then awk '!/colorscheme/' " ..
-            init_file .. " > temp && mv temp " .. init_file .. "; fi && echo '" .. init_cmd .. "' >> " .. init_file)
-        actions.close(bufnr)
+  local function enter(bufnr)
+    local selected = actions_state.get_selected_entry()
+    local cmd = 'colorscheme ' .. selected[1]
+    vim.cmd(cmd)
+    local init_cmd = 'vim.cmd("' .. cmd .. '")'
+    local init_file = vim.fn.expand(vim.fn.stdpath('config') .. "/init.lua")
+
+    local function modify_file()
+      local file = io.open(init_file, "r")
+      if not file then return end
+      local content = file:read("*all")
+      file:close()
+
+      content = content:gsub("vim.cmd%('colorscheme'.-%)\n?", "")
+      local file = io.open(init_file, "w")
+      if not file then return end
+      file:write(content .. "\n" .. init_cmd .. "\n")
+      file:close()
     end
 
-    local function next_color(bufnr)
-        actions.move_selection_next(bufnr)
-        local selected = actions_state.get_selected_entry()
-        local cmd = 'colorscheme ' .. selected[1]
-        vim.cmd(cmd)
-    end
+    modify_file()
+    actions.close(bufnr)
+  end
 
-    local function prev_color(bufnr)
-        actions.move_selection_previous(bufnr)
-        local selected = actions_state.get_selected_entry()
-        local cmd = 'colorscheme ' .. selected[1]
-        vim.cmd(cmd)
-    end
+  local function next_color(bufnr)
+    actions.move_selection_next(bufnr)
+    local selected = actions_state.get_selected_entry()
+    local cmd = 'colorscheme ' .. selected[1]
+    vim.cmd(cmd)
+  end
 
-    local installedColors = vim.fn.getcompletion("", "color")
+  local function prev_color(bufnr)
+    actions.move_selection_previous(bufnr)
+    local selected = actions_state.get_selected_entry()
+    local cmd = 'colorscheme ' .. selected[1]
+    vim.cmd(cmd)
+  end
 
-    pickers.new(opts, {
-        finder = finders.new_table(installedColors),
-        sorter = sorters.get_generic_fuzzy_sorter({}),
+  local installedColors = vim.fn.getcompletion("", "color")
 
-        attach_mappings = function(_, map)
-            actions.select_default:replace(enter)
+  pickers.new(opts, {
+    finder = finders.new_table(installedColors),
+    sorter = sorters.get_generic_fuzzy_sorter({}),
 
-            map("i", "<C-j>", next_color)
-            map("i", "<C-k>", prev_color)
-            map("i", "<C-n>", next_color)
-            map("i", "<C-p>", prev_color)
-            return true
-        end,
-    }):find()
+    attach_mappings = function(_, map)
+      actions.select_default:replace(enter)
+
+      map("i", "<C-j>", next_color)
+      map("i", "<C-k>", prev_color)
+      map("i", "<C-n>", next_color)
+      map("i", "<C-p>", prev_color)
+      return true
+    end,
+  }):find()
 end
 
 return M
